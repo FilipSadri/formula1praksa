13842ca2c7008a75768afb48202a97de41f3efb0
Filip Sadri <filipsadri@gmail.com>
2022-06-27 11:04:50 +0200

    dodate 3 komponente

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Drivers from "./components/Drivers";
+import Teams from "./components/Teams";
+import Races from "./components/Teams";
 
 
 export default class App extends React.Component {
---
+++ src/components/Races.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class Races extends React.Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
