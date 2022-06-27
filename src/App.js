d613a588f1c1c2e6abf66ef9a3573b0aca84a77b
Filip Sadri <filipsadri@gmail.com>
2022-06-27 12:54:18 +0200

    Zapoceta Teams komponenta

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Drivers from "./components/Drivers";
 import Teams from "./components/Teams";
 import Races from "./components/Teams";
 
-
 export default class App extends React.Component {
-
-    
 
     render() {
         return (
-           <div>Formula 1</div>
+            <div>
+                <div>Formula 1</div>
+                <Teams />
+            </div>
         );
     };
 }
--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,6 +1,25 @@
 import React from "react";
+import * as $ from "jquery";
 
 export default class Teams extends React.Component{
+
+    state = {
+        teams: [],
+    }
+
+    componentDidMount() {
+        this.getPosts();
+    }
+
+    getPosts = () => {
+        const url = "http://ergast.com/api/f1/2013/constructorStandings.json";
+        $.get(url, (data) => {
+            console.log("data", data);
+            this.setState({
+                teams: data
+            });
+        });
+    }
     render(){
         return(
             <div></div>
