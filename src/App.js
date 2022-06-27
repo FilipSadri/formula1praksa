d2f440734e72fcc15ca0146f74e5073baa760d42
Strahinja Dimic <dimicstrahinja@gmail.com>
2022-06-27 12:42:11 +0200

    Zapoceta Drivers komponenta

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
 
     render() {
         return (
-           <div>Formula 1</div>
+           <div><Drivers/></div>
         );
     };
 }
--- src/components/Drivers.js
+++ src/components/Drivers.js
@@ -1,10 +1,32 @@
 import React from "react";
+import * as $ from "jquery"
 
 export default class Drivers extends React.Component {
+
+    state = {
+        drivers:[]
+        
+    }
+
+    componentDidMount(){
+        this.getDrivers()
+    }
+
+    getDrivers = () => {
+        const url = "http://ergast.com/api/f1/2022/driverStandings.json"
+        $.get(url, (data)=>{
+            console.log("data", data)
+            this.setState({
+                drivers: data
+            })
+        })
+    }
+
     render(){
         return(
-            <div></div>
+            <div>
+               {this.state.map(driver)}
+            </div>
         )
     }
 }
-
