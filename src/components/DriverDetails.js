789617ab1a6eb976c347a819a539056a32946cc6
Filip Sadri <filipsadri@gmail.com>
2022-06-28 15:18:05 +0200

    Dodata async metoda u DriverDitails

--- src/components/DriverDetails.js
+++ src/components/DriverDetails.js
@@ -1,62 +1,67 @@
 import React from 'react';
 import * as $ from 'jquery';
 
-
 export default class DriverDetails extends React.Component {
 
     state = {
-        details: [
-
-        ],
-         races: [
-
-         ]
-
-        
+        details: [],
+        races: []
     }
 
-    componentDidMount (){
-        this.getDriverDetails()
-        this.getRaceDetails()
+    componentDidMount() {
+        this.getDriverDetails();
     }
 
-    getDriverDetails = ()=>{
+    getDriverDetails = async () => {
         const id = this.props.match.params.driverId;
-        console.log("id", this.props.match.params.driverId)
-        const url= `http://ergast.com/api/f1/2013/drivers/${id}/driverStandings.json`;
-        $.get(url, (data)=>{
-            // console.log("data",data)
-            this.setState({
-                details: data.MRData.StandingsTable.StandingsLists[0].DriverStandings
-            })
+        const driverStandingsUrl = `http://ergast.com/api/f1/2013/drivers/${id}/driverStandings.json`;
+        const resultsUrl = `http://ergast.com/api/f1/2013/drivers/${id}/results.json`;
+        const responseDriverStandings = await fetch(driverStandingsUrl);
+        const driverStandings = await responseDriverStandings.json();
+        const responseResults = await fetch(resultsUrl);
+        const results = await responseResults.json();
+        this.setState({
+            details: driverStandings.MRData.StandingsTable.StandingsLists[0].DriverStandings,
+            races: results.MRData.RaceTable.Races
         })
     }
 
-    getRaceDetails = ()=>{
-        const id = this.props.match.params.driverId;
-        const url= `http://ergast.com/api/f1/2013/drivers/${id}/results.json`
-        $.get(url, (data)=>{
-            // console.log("data", data)
-            this.setState({
-                races: data.MRData.RaceTable.Races
-            })
-        })
-    }
+    // getDriverDetails = () => {
+    //     const id = this.props.match.params.driverId;
+    //     console.log("id", this.props.match.params.driverId)
+    //     const url = `http://ergast.com/api/f1/2013/drivers/${id}/driverStandings.json`;
+    //     $.get(url, (data) => {
+    //         // console.log("data",data)
+    //         this.setState({
+    //             details: data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+    //         })
+    //     })
+    // }
+
+    // getRaceDetails = () => {
+    //     const id = this.props.match.params.driverId;
+    //     const url = `http://ergast.com/api/f1/2013/drivers/${id}/results.json`
+    //     $.get(url, (data) => {
+    //         // console.log("data", data)
+    //         this.setState({
+    //             races: data.MRData.RaceTable.Races
+    //         })
+    //     })
+    // }
 
     render() {
         console.log("races", this.state.races)
         return (
-            
-            <div>  
-                {this.state.details.map(detail=>{
+            <div>
+                {this.state.details.map(detail => {
                     return (
-                    <div key={detail.Driver.driverId}>
-                        <h3>Driver details</h3>
-                        <p>Full Name:{detail.Driver.givenName} {detail.Driver.familyName}</p>
-                        <p>Birthday:{detail.Driver.dateOfBirth}</p>
-                        <p>Country:{detail.Driver.nationality}</p>  
-                    </div>
-                )           
+                        <div key={detail.Driver.driverId}>
+                            <h3>Driver details</h3>
+                            <p>Full Name:{detail.Driver.givenName} {detail.Driver.familyName}</p>
+                            <p>Birthday:{detail.Driver.dateOfBirth}</p>
+                            <p>Country:{detail.Driver.nationality}</p>
+                        </div>
+                    )
                 })}
 
                 <table border={1}>
@@ -74,7 +79,7 @@ export default class DriverDetails extends React.Component {
                     </thead>
 
                     <tbody>
-                        {this.state.races.map(race=>
+                        {this.state.races.map(race =>
                             <tr key={race.round}>
                                 <td>{race.round}</td>
                                 <td>{race.raceName}</td>
@@ -82,10 +87,10 @@ export default class DriverDetails extends React.Component {
                                 <td>{race.Results[0].grid}</td>
                                 <td>{race.Results[0].position}</td>
                             </tr>
-                        )}             
-                    </tbody>        
-                </table>              
+                        )}
+                    </tbody>
+                </table>
             </div>
-       );
+        );
     }
 }
