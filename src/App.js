44c781ab55c9b891547b4e48608a9d3ff2f4f8ee
Strahinja Dimic <dimicstrahinja@gmail.com>
2022-06-28 09:30:42 +0200

    Izmenjen App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import Drivers from "./components/Drivers";
 import Teams from "./components/Teams";
-import Races from "./components/Teams";
+import Races from "./components/Races";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 export default class App extends React.Component {
 
     render() {
         return (
-            //    <div><Drivers/></div>
-
             <div className="container">
-                <div><Races/></div>
                 <Router>
                     <div>
                         <nav>
@@ -20,17 +17,17 @@ export default class App extends React.Component {
                                     <Link to="/">Drivers</Link>
                                 </li>
                                 <li>
-                                    <Link to="/">Races</Link>
+                                    <Link to="/teams">Teams</Link>
                                 </li>
                                 <li>
-                                    <Link to="/">Contact</Link>
+                                    <Link to="/races">Races</Link>
                                 </li>
                             </ul>
                         </nav>
                         <Routes>
-                            {/* <Route path="/" element={<Drivers />} /> */}
-                            {/* <Route path="/races" element={<Races />} /> */}
-                            {/* <Route path="/teams" element={<Teams />} /> */}
+                            <Route path="/" element={<Drivers />} />
+                            <Route path="/teams" element={<Teams />} />
+                            <Route path="/races" element={<Races />} />
                         </Routes>
                     </div>
                 </Router>
