a7b2baf3ca89990b4efeb4234c02f3ecf4aefcbf
Filip Sadri <filipsadri@gmail.com>
2022-06-29 15:21:19 +0200

    Add route u app

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ export default class App extends React.Component {
                             <Route path="/driverDetails/:driverId" exact component={DriverDetails}/>
                             <Route path="http://en.wikipedia.org/wiki/Sebastian_Vettel" exact component={DriverDetails}/>
                             <Route path="/teams" exact component={Teams} />
+                            <Route path="/teamDetails/:constructorId" exact component={TeamDetails}/>
                             <Route path="/races" exact component={Races} /> 
                             <Route path="/raceResults/:circuitId" exact component={RaceResults} />
 
